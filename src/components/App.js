c06a37206a02d26fb2367519c3e8f998955be0fa
Mati Joseph <crucialniccur@gmail.com>
2025-04-11 12:41:23 +0300

    add props, image and about to the About component in App parent

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header name={blogData.name} />
-      <About />
+      <About image={blogData.image} about={blogData.about} />
     </div>
   );
 }
