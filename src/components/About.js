f8acea1463eb490b60d670a0e2cabe97848e357b
Mati Joseph <crucialniccur@gmail.com>
2025-04-11 12:47:02 +0300

    add the alt text to the image

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const About = ({ image = "https://via.placeholder.com/215", about }) => {
+const About = ({ image = "https://via.placeholder.com/215", about, alt }) => {
   return (
     <aside>
-      <img src={image} alt={about} />
+      <img src={image} alt={alt} />
       <p>{about}</p>
     </aside>
   );
